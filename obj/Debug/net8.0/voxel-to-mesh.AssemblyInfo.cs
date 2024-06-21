0267d9c3112418af729111806298fe0b5cf6f271
E-moL0587 <kai.pika.025@outlook.com>
2024-06-21 14:35:37 +0900

    update

--- obj/Debug/net8.0/voxel-to-mesh.AssemblyInfo.cs
+++ obj/Debug/net8.0/voxel-to-mesh.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("voxel-to-mesh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb8fc0280c2ab2b8021a9b33ac03386b5e4b0f30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bc79763b7ac222288dee5d7cda149ffd0fd7666")]
 [assembly: System.Reflection.AssemblyProductAttribute("voxel-to-mesh")]
 [assembly: System.Reflection.AssemblyTitleAttribute("voxel-to-mesh")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
