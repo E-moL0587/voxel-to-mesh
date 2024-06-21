c0d5d4aa94905cac6c9a074b09d1ea78292bb326
E-moL0587 <kai.pika.025@outlook.com>
2024-06-21 14:47:17 +0900

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,73 +6,73 @@ using System.Text;
 using System.Text.Json;
 
 namespace voxel_to_mesh.Controllers {
-    public class HomeController : Controller {
-        public IActionResult Index() => View();
+  public class HomeController : Controller {
+    public IActionResult Index() => View();
 
-        public IActionResult Pixels() {
-            var frontImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/front.png");
-            var sideImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/side.png");
+    public IActionResult Pixels() {
+      var frontImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/front.png");
+      var sideImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/side.png");
 
-            var (frontBase64Image, frontBinaryData) = ProcessImage(frontImagePath);
-            ViewData["FrontBinaryImage"] = frontBase64Image;
-            ViewData["FrontBinaryData"] = frontBinaryData;
+      var (frontBase64Image, frontBinaryData) = ProcessImage(frontImagePath);
+      ViewData["FrontBinaryImage"] = frontBase64Image;
+      ViewData["FrontBinaryData"] = frontBinaryData;
 
-            var (sideBase64Image, sideBinaryData) = ProcessImage(sideImagePath);
-            ViewData["SideBinaryImage"] = sideBase64Image;
-            ViewData["SideBinaryData"] = sideBinaryData;
+      var (sideBase64Image, sideBinaryData) = ProcessImage(sideImagePath);
+      ViewData["SideBinaryImage"] = sideBase64Image;
+      ViewData["SideBinaryData"] = sideBinaryData;
 
-            return View();
-        }
-
-        private static (string base64Image, string binaryData) ProcessImage(string imagePath) {
-            using var image = Image.Load<Rgba32>(imagePath);
-            image.Mutate(x => x.Resize(30, 30).Grayscale().BinaryThreshold(0.5f));
-
-            using var ms = new MemoryStream();
-            image.SaveAsPng(ms);
-            var base64Image = Convert.ToBase64String(ms.ToArray());
-
-            var binaryData = new StringBuilder(image.Height * image.Width);
-            for (int y = 0; y < image.Height; y++) {
-                for (int x = 0; x < image.Width; x++) {
-                    binaryData.Append(image[x, y].R == 255 ? '1' : '0');
-                }
-            }
-
-            return ($"data:image/png;base64,{base64Image}", binaryData.ToString());
-        }
-
-        private static List<int[]> GenerateVoxelData(string frontData, string sideData, int width, int height) {
-            var voxelData = new List<int[]>();
-
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    int z = x;
-                    int frontIndex = y * width + x;
-                    int sideIndex = y * width + z;
-
-                    if (frontData[frontIndex] == '1' && sideData[sideIndex] == '1') {
-                        voxelData.Add(new int[] { x, y, z });
-                    }
-                }
-            }
-
-            return voxelData;
-        }
-
-        public IActionResult Voxel() {
-            var frontImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/front.png");
-            var sideImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/side.png");
-
-            var (_, frontBinaryData) = ProcessImage(frontImagePath);
-            var (_, sideBinaryData) = ProcessImage(sideImagePath);
-
-            var voxelData = GenerateVoxelData(frontBinaryData, sideBinaryData, 30, 30);
-            ViewData["VoxelData"] = JsonSerializer.Serialize(voxelData);
-
-            return View();
-        }
-
-        public IActionResult Mesh() => View();
+      return View();
     }
+
+    private static (string base64Image, string binaryData) ProcessImage(string imagePath) {
+      using var image = Image.Load<Rgba32>(imagePath);
+      image.Mutate(x => x.Resize(30, 30).Grayscale().BinaryThreshold(0.5f));
+
+      using var ms = new MemoryStream();
+      image.SaveAsPng(ms);
+      var base64Image = Convert.ToBase64String(ms.ToArray());
+
+      var binaryData = new StringBuilder(image.Height * image.Width);
+      for (int y = 0; y < image.Height; y++) {
+        for (int x = 0; x < image.Width; x++) {
+          binaryData.Append(image[x, y].R == 255 ? '1' : '0');
+        }
+      }
+
+      return ($"data:image/png;base64,{base64Image}", binaryData.ToString());
+    }
+
+    private static List<int[]> GenerateVoxelData(string frontData, string sideData, int width, int height) {
+      var voxelData = new List<int[]>();
+
+      for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+          int z = x;
+          int frontIndex = y * width + x;
+          int sideIndex = y * width + z;
+
+          if (frontData[frontIndex] == '1' && sideData[sideIndex] == '1') {
+            voxelData.Add(new int[] { x, y, z });
+          }
+        }
+      }
+
+      return voxelData;
+    }
+
+    public IActionResult Voxel() {
+      var frontImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/front.png");
+      var sideImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/side.png");
+
+      var (_, frontBinaryData) = ProcessImage(frontImagePath);
+      var (_, sideBinaryData) = ProcessImage(sideImagePath);
+
+      var voxelData = GenerateVoxelData(frontBinaryData, sideBinaryData, 30, 30);
+      ViewData["VoxelData"] = JsonSerializer.Serialize(voxelData);
+
+      return View();
+    }
+
+    public IActionResult Mesh() => View();
+  }
 }
