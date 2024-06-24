5f340d4b708a3d9bf45b2cd745fd3621510190c4
E-moL0587 <kai.pika.025@outlook.com>
2024-06-24 12:51:26 +0900

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,10 +22,6 @@ namespace voxel_to_mesh.Controllers {
     }
 
     public IActionResult Index() {
-      return View();
-    }
-
-    public IActionResult Pixels() {
       var frontBinaryData = _imageProcessor.ProcessImage(_frontImagePath);
       var sideBinaryData = _imageProcessor.ProcessImage(_sideImagePath);
       var topBinaryData = _imageProcessor.ProcessImage(_topImagePath);
@@ -34,6 +30,13 @@ namespace voxel_to_mesh.Controllers {
       ViewData["SideBinaryData"] = sideBinaryData;
       ViewData["TopBinaryData"] = topBinaryData;
 
+      var (voxelData, _, _, _) = PrepareData();
+      var meshData = _marchingCubes.GenerateMesh(voxelData, 20, 20, 20);
+      var smoothedMeshData = _laplacianSmoothing.Smooth(meshData);
+
+      ViewData["VoxelData"] = JsonSerializer.Serialize(voxelData);
+      ViewData["MeshData"] = JsonSerializer.Serialize(meshData);
+      ViewData["SmoothData"] = JsonSerializer.Serialize(smoothedMeshData);
       return View();
     }
 
@@ -41,36 +44,9 @@ namespace voxel_to_mesh.Controllers {
       var frontBinaryData = _imageProcessor.ProcessImage(_frontImagePath);
       var sideBinaryData = _imageProcessor.ProcessImage(_sideImagePath);
       var topBinaryData = _imageProcessor.ProcessImage(_topImagePath, 90);
-
       var voxelData = _voxelGenerator.GenerateVoxelData(frontBinaryData, sideBinaryData, topBinaryData, 20);
 
       return (voxelData, frontBinaryData, sideBinaryData, topBinaryData);
-    }
-
-    public IActionResult Voxel() {
-      var (voxelData, _, _, _) = PrepareData();
-      ViewData["VoxelData"] = JsonSerializer.Serialize(voxelData);
-
-      return View();
-    }
-
-    public IActionResult Mesh() {
-      var (voxelData, _, _, _) = PrepareData();
-      var meshData = _marchingCubes.GenerateMesh(voxelData, 20, 20, 20);
-
-      ViewData["MeshData"] = JsonSerializer.Serialize(meshData);
-
-      return View();
-    }
-
-    public IActionResult Smooth() {
-      var (voxelData, _, _, _) = PrepareData();
-      var meshData = _marchingCubes.GenerateMesh(voxelData, 20, 20, 20);
-      var smoothedMeshData = _laplacianSmoothing.Smooth(meshData);
-
-      ViewData["SmoothData"] = JsonSerializer.Serialize(smoothedMeshData);
-
-      return View();
     }
   }
 }
